cfc6f91cc9dc946df113f82aac0b6335514da57b
enwalker33 <44973965+enwalker33@users.noreply.github.com>
2025-01-13 09:57:49 -0600

    Example London parking lot cordinates to mark on map

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,28 @@
+// intialize the google map
 function myMap() {
+    // Set the center of the map (latitude, longitude) and the zoom level
     var mapProp = {
-        center: new google.maps.LatLng(51.508742, -0.120850),
+        center: new google.maps.LatLng(51.508742, -0.120850), // London Eye coordinates as an example
         zoom: 5,
     };
+    //Create the map
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+
+    // Create an array of parking spots
+    var parkingSpots = [
+        { lat: 51.508742, lng: -0.120850, name: "London Eye" },
+        { lat: 51.508742, lng: -0.120850, name: "Big Ben" },
+        { lat: 51.508742, lng: -0.120850, name: "Tower Bridge" },
+        { lat: 51.508742, lng: -0.120850, name: "Buckingham Palace" },
+        { lat: 51.508742, lng: -0.120850, name: "The Shard" },
+
+    ];
+    // Loop through each parking spot and add a marker to the map
+    parkingSpots.forEach(function (parkingSpot) {
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(parkingSpot.lat, parkingSpot.lng),
+            map: map,
+            title: parkingSpot.name,
+        });
+    });
 }
