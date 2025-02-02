00577e84ec555c16c8eef11ab34cb12a65a64dc3
enwalker33 <44973965+enwalker33@users.noreply.github.com>
2025-02-02 16:57:04 -0600

    update login.html and script.js

--- public/js/script.js
+++ public/js/script.js
@@ -87,4 +87,16 @@ function myMap() {
             title: parkingSpot.name,
         });
     });
+    // Optional: Link for showing/hiding password
+
+      document.getElementById('toggle-password').addEventListener('click', function() {
+        const passwordField = document.getElementById('password');
+        if (passwordField.type === 'password') {
+          passwordField.type = 'text';
+          this.textContent = 'Hide';
+        } else {
+          passwordField.type = 'password';
+          this.textContent = 'Show';
+        }
+      });
 }
